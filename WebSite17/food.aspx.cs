11016d81803f7007a43e3b2b10f7bc25b18d5927
hayer harwinder <happyhayer20@gmail.com>
2019-08-21 14:21:06 -0400

    c# of food page

---
+++ WebSite17/food.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class food : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("mexican.aspx");
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("indian.aspx");
+
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("italian.aspx");
+    }
+}
