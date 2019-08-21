90a7fe591e9be04276d097998fd47469d43837ce
hayer harwinder <happyhayer20@gmail.com>
2019-08-21 09:18:32 -0400

    review page created Gagan & Harwinder

---
+++ WebSite17/Review.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Home.master" AutoEventWireup="true" CodeFile="Review.aspx.cs" Inherits="Review" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+
+ <div  class="signinn">
+
+                <div class="">
+
+              <p><h3>Review</h3></p> 
+
+                <div>
+
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" 
+            ErrorMessage="Leave review here" ControlToValidate="TextBox1" ForeColor="Red"></asp:RequiredFieldValidator>
+   
+    </div>
+
+                   
+    <div class="">
+        
+    
+    </div>
+    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+     
+    </div>
+   <asp:Button ID="Button1" runat="server" Text="Submit" onclick="Button1_Click" />
+
+
+
+
+   </div>
+
+
+</asp:Content>
+
---
+++ WebSite17/Review.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class Review : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SqlConnection conn = new SqlConnection("Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\hp\\Documents\\Visual Studio 2010\\WebSites\\github work\\WebSite17\\App_Data\\Database.mdf;Integrated Security=True;User Instance=True");
+        SqlCommand cmd;
+        string st;
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SqlCommand cmd;
+        string st;
+        SqlConnection conn = new SqlConnection("Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\Users\\hp\\Documents\\Visual Studio 2010\\WebSites\\github work\\WebSite17\\App_Data\\Database.mdf;Integrated Security=True;User Instance=True");
+        conn.Open();
+        st = "insert review(review) values('" + TextBox1.Text + "')";
+        cmd = new SqlCommand(st, conn);
+
+        cmd.ExecuteNonQuery();
+        Response.Redirect("Main.aspx");
+    }
+}
